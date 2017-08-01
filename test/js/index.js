7eda63dc81382766cfce1d4f319f222e7c702ed0
Ankit Karnany <ankit@greylabs.org>
2017-08-01 14:21:48 +0530

    implemented basic auto complete UI with dummy data

--- test/js/index.js
+++ test/js/index.js
@@ -2,6 +2,17 @@ var indexApp = angular.module("indexApp", []);
 
 indexApp.controller('indexCtrl', ['$scope', function($scope) {
     $scope.button = "This is cool!!!";
+
+    $scope.searchList = ['hello', 'world'];
+
+    $scope.items = [];
+
+    $scope.search = function (s) {
+        $scope.items = [];
+        $scope.items = $scope.searchList.filter(function (ls) {
+            return s == ls;
+        });
+    }
 }]);
 
 angular.element(document).ready(function () {
